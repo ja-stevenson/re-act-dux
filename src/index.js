2295aeff839cf262f24138df5ab57bd212c81665
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-12 02:01:45 -0600

    react starter bones added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.container')
+);
