60fcafff7b762c6aae79cbd83c847604fbfa4250
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-15 16:19:59 -0600

    embedded video and side panel videos are clickable

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import YTSearch from 'youtube-api-search'
 import API_KEY from './keys/api';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'cheese'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+        });
     });
   }
 
@@ -20,7 +27,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
