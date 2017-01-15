35c8fcb7b5e67d70a000f822918d47ed0f7c9d0f
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-15 14:59:31 -0600

    youtube search feature added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 import API_KEY from './keys/api';
 import SearchBar from './components/search_bar';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
