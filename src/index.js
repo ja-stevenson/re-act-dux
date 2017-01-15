9f8abbc47024edc0714eea65f07f4db538ab51c4
Jimmy Stevenson <jimmyastevenson@gmail.com>
2017-01-15 11:55:31 -0600

    added basic search component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from './keys/api';
+import SearchBar from './components/search_bar';
 
-const App = function() {
-  return <div>Hi!</div>
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(
